500415fd73caa1b81c378ad4c999ed09592a95db
Ayushi Agarwal <ayushiagarwal2312@gmail.com>
2024-02-11 14:36:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world from express');
+})
+
+app.listen(3500, () => {
+    console.log('hello')
+})
